92f6caae02457d1fc53a60774eb54c32a796888e
Ravi Kasha <rkasha@vmware.com>
2015-10-28 21:01:24 +0530

    --Changed email Service dependencies for sending the email

--- src/main/java/com/vmware/scheduler/domain/EmailService.java
+++ src/main/java/com/vmware/scheduler/domain/EmailService.java
@@ -1,6 +1,10 @@
 package com.vmware.scheduler.domain;
 
-import javax.mail.*;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
